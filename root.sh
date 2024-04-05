d7902e96cd7975e9313c8cd4af11a37de84210cf
passeway <164740303+passeway@users.noreply.github.com>
2024-04-06 00:57:31 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-echo root:Liulu19950908! sudo chpasswd root
-sudo sed -i 's/^?permitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd config;
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g’ /etc/ssh/sshd config;
+echo root:Liulu19950908! | sudo chpasswd
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo service sshd restart
