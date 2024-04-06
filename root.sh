57c5801fbabac4549926c2f4373731ba5dbeb1f8
passeway <164740303+passeway@users.noreply.github.com>
2024-04-06 18:50:47 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -3,7 +3,7 @@
 # 函数：检查错误并退出
 check_error() {
     if [ $? -ne 0 ]; then
-        echo "发生错误。退出..." >&2
+        echo "发生错误。退出..."
         exit 1
     fi
 }
@@ -12,20 +12,18 @@ check_error() {
 generate_random_password() {
     # 检查是否有root权限
     if [ "$(id -u)" -ne 0 ]; then
-        echo "需要root权限来设置密码。退出..." >&2
+        echo "需要root权限来设置密码。退出..."
         exit 1
     fi
 
-    # 生成16位随机密码，包含大小写字母、数字和特殊字符
-    random_password=$(openssl rand -base64 16 | tr -dc 'a-zA-Z0-9!@#$%^&*()_-')
+    random_password=$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9!@#$%^&*()-_=+{}[]|\;:,.<>?')
     echo "root:$random_password" | sudo chpasswd
     check_error
-    echo "$random_password" >&2 # 输出密码到标准错误流
+    echo "$random_password" # 输出密码
 }
 
 # 修改 sshd_config 文件
 modify_sshd_config() {
-    # 备份原始的sshd配置文件
     sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
     check_error
 
@@ -46,19 +44,15 @@ modify_sshd_config() {
 
 # 重启 SSHD 服务
 restart_sshd_service() {
-    # 重启SSH服务
     sudo service sshd restart
-    if [ $? -ne 0 ]; then
-        echo "重启 SSH 服务失败。退出..." >&2
-        exit 1
-    fi
+    check_error
 }
 
 # 提示用户选择密码选项
 echo "请选择密码选项："
 echo "1. 生成密码"
 echo "2. 输入密码"
-read -p "请输入选项编号： " option
+read -p "请输入选项： " option
 
 case $option in
     1)
@@ -71,7 +65,7 @@ case $option in
         password=$custom_password # 保存输入的密码
         ;;
     *)
-        echo "无效选项。退出..." >&2
+        echo "无效选项。退出..."
         exit 1
         ;;
 esac
@@ -79,8 +73,9 @@ esac
 modify_sshd_config
 restart_sshd_service
 
-echo "密码更改成功：$password" >&2 # 输出密码到标准错误流
 echo "操作成功完成。" # 输出成功消息
+echo "密码更改成功：$password" >&2 # 输出密码到标准错误流
+
 
 # 删除下载的脚本
 rm -f "$0"
